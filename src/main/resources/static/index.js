dfb2a0b089859ddda868b410a8e0beffe04091b2
KorEugene <johnkorch@yandex.ru>
2021-08-18 20:42:14 +0300

    hw8 ready

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,9 +1,9 @@
 angular.module('market-front', []).controller('indexController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/market/';
 
-    $scope.firstPage = 1;
-    $scope.currentPage = $scope.firstPage;
-    $scope.lastPage = 1;
+    $scope.firstPageIndex = 1;
+    $scope.lastPageIndex = 1;
+    $scope.currentPage = 1;
 
     // $scope.loadProducts = function () {
     //     $http.get(contextPath + 'products')
@@ -12,6 +12,16 @@ angular.module('market-front', []).controller('indexController', function ($scop
     //             $scope.productsPage = response.data;
     //         });
     // };
+
+    $scope.changePage = function (offset) {
+        let nextPageIndex = $scope.currentPage + offset;
+        if (nextPageIndex < $scope.firstPageIndex || nextPageIndex > $scope.lastPageIndex) {
+            $scope.loadProducts($scope.currentPage)
+        } else {
+            $scope.currentPage = nextPageIndex;
+            $scope.loadProducts($scope.currentPage)
+        }
+    }
 
     $scope.loadProducts = function (pageIndex = 1) {
         $http({
@@ -22,11 +32,7 @@ angular.module('market-front', []).controller('indexController', function ($scop
             }
         }).then(function (response) {
             console.log(response);
-            // if (pageIndex >= 1 || pageIndex <= response.data.totalPages) {
-            $scope.currentPage = pageIndex;
-            // }
-            // console.log(response.data.first)
-            // console.log(response.data.last)
+            $scope.lastPageIndex = response.data.totalPages;
             $scope.productsPage = response.data;
         });
     };
