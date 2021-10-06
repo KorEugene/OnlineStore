6f96309b9a4f0f8dba1d20bbfee563bade0715b6
KorEugene <johnkorch@yandex.ru>
2021-10-06 20:07:30 +0300

    CartTest + AuthControllerTest

---
+++ src/test/java/ru/geekbrains/springwebappjs/controllers/AuthControllerTest.java
@@ -0,0 +1,73 @@
+package ru.geekbrains.springwebappjs.controllers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.hamcrest.Matchers.hasKey;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void badAuthTokenRequest() throws Exception {
+        String jsonBadRequest = "{\n" +
+                "\t\"username\": \"bob\",\n" +
+                "\t\"password\": \"100\"\n" +
+                "}";
+        MvcResult result = mockMvc.perform(
+                        post("/auth")
+                                .content(jsonBadRequest)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+
+        Assertions.assertEquals(401, result.getResponse().getStatus());
+    }
+
+    @Test
+    void createAuthToken() throws Exception {
+        String jsonRequest = "{\n" +
+                "\t\"username\": \"user\",\n" +
+                "\t\"password\": \"user\"\n" +
+                "}";
+        MvcResult result = mockMvc.perform(
+                        post("/auth")
+                                .content(jsonRequest)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Assertions.assertEquals(200, result.getResponse().getStatus());
+    }
+
+    @Test
+    public void securityAccessDeniedTest() throws Exception {
+        mockMvc.perform(get("/api/v1/orders"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void securityAccessAllowedTest() throws Exception {
+        mockMvc.perform(get("/api/v1/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isNotEmpty())
+                .andExpect(jsonPath("$.pageable").isMap())
+                .andExpect(jsonPath("$.pageable", hasKey("pageSize")));
+    }
+}
